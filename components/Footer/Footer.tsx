64d9ff88d36a3675e6dddde03d5d0567aed5f3b1
SudoDosu <93739034+SudodoSu@users.noreply.github.com>
2024-07-31 18:46:07 +0200

    style

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -58,7 +58,7 @@ function Footer() {
           <ul className="space-y-4">
             <li>Alen Smoljan</li>
             <li>Tršičani 43, 52215, Vodnjan</li>
-            <li>+(385) 98 263 136</li>
+            {/* <li>+(385) 98 263 136</li> */}
             <li>villpanoramaistria@gmail.com</li>
           </ul>
         </div>
